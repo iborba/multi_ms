7bace5669cbefecc9d5ffe8a700725f5be955e6e
Israel Borba <israel.borba@totvs.com.br>
2019-12-10 14:23:19 -0300

    Kube removido

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-cd ./ms1 && npm run docker && kubectl apply -f k8/deployment.yaml
-cd ../ms2 && npm run docker && kubectl apply -f k8/deployment.yaml
-cd ../ms3 && npm run docker && kubectl apply -f k8/deployment.yaml
-cd ../ms4 && npm run docker && kubectl apply -f k8/deployment.yaml
-cd ../ms5 && npm run docker && kubectl apply -f k8/deployment.yaml
-cd ../ms6 && npm run docker && kubectl apply -f k8/deployment.yaml
